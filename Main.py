cc95aa009e362e47b8510b644be66e57f59e127f
wanCwC2 <WanPcwc0630@gmail.com>
2022-12-24 14:35:19 +0800

    Run CNN

--- Main.py
+++ Main.py
@@ -23,7 +23,6 @@ lesion_type_dict = {
 pd.Categorical(lesion_type_dict).codes
 
 load_img = pd.read_csv('data/project3_train.csv')
-img_test = pd.read_csv('data/project3_test.csv')
 
 #iloc選取特定範圍，讀取種類編號
 X_img , y_label = load_img.iloc[: , :-1].values , load_img.iloc[: , -1].values
@@ -43,7 +42,67 @@ X_img_train_normalize = X_img_train.astype('float32') / 255.0
 #使用np_utils.to_categorical()傳入各參數的label標籤欄位，再執行OneHot encoding (轉成0或1的組合)
 y_label_train_OneHot = np_utils.to_categorical(y_label)
 
-#使用np_utils.to_categorical()傳入各參數的label標籤欄位，再執行OneHot encoding (轉成0或1的組合)
-y_label_train_OneHot = np_utils.to_categorical(y_label)
+x_train, x_test, y_train, y_test = train_test_split(X_img_train_normalize, y_label, test_size = 0.3)
 
-X_img_normalize = X_img.astype('float32') / 255.0
+#匯入keras中的Sequential、layers模組(Dense、 Dropout、 Activation、 Flatten、Conv2D、 MaxPooling2D、 ZeroPadding2D)
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+# 建立簡單的線性執行的模型
+model = Sequential()
+# 建立卷積層，filter=32,即 output space 的深度, Kernal Size: 3x3, activation function 採用 relu
+model.add(Conv2D(filters = 8,
+                 kernel_size = (3, 3),
+                 activation = 'relu',
+                 input_shape = [28,28,3],
+                 padding = 'same'))
+# 建立卷積層，filter=64,即 output size, Kernal Size: 3x3, activation function 採用 relu
+#model.add(Conv2D(64, (3, 3), activation='relu'))
+# 建立池化層，池化大小=2x2，取最大值
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(16, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Conv2D(64, (3, 3), activation='relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+# Flatten層把多維的輸入一維化，常用在從卷積層到全連接層的過渡。
+model.add(Flatten())
+# 全連接層: 128個output
+model.add(Dense(16, activation='relu'))
+model.add(Dense(8, activation='relu'))
+# Dropout層隨機斷開輸入神經元，用於防止過度擬合，斷開比例:0.5
+#model.add(Dropout(0.5))
+# 使用 softmax activation function，將結果分類
+model.add(Dense(1, activation='softmax'))
+
+# 編譯: 選擇損失函數、優化方法及成效衡量方式
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# 進行訓練, 訓練過程會存在 train_history 變數中
+train_history = model.fit(x_train, y_train,
+                          validation_split = 0.2,
+                          batch_size = 10,
+                          epochs = 12,
+                          verbose = 1)
+
+# 顯示損失函數、訓練成果(分數)
+score = model.evaluate(x_test, y_test, verbose = 0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# Design your CNN model
+# 使用最後的模型進行測試資料預測
+load_test_img = pd.read_csv('data/project3_test.csv')
+img_test = load_test_img.values
+x_test=img_test.reshape(img_test.shape[0],28,28,3)
+x_test_normalize = x_test.astype('float32') / 255.0
+df_submit = pd.DataFrame([], columns=['Id', 'Label'])
+df_submit['Id'] = [f'{i:04d}' for i in range(len(x_test_normalize))]
+df_submit['Label'] = np.argmax(model.predict(x_test_normalize), axis=-1)
+df_submit.to_csv('submission_CNN.csv', index=None)
