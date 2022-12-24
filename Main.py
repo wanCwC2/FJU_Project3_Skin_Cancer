a00d98c920b954744de89b1242e7cef4168c80bc
wanCwC2 <WanPcwc0630@gmail.com>
2022-12-24 15:40:36 +0800

    Use CNN 0.7437

--- Main.py
+++ Main.py
@@ -54,7 +54,7 @@ from keras import backend as K
 # 建立簡單的線性執行的模型
 model = Sequential()
 # 建立卷積層，filter=32,即 output space 的深度, Kernal Size: 3x3, activation function 採用 relu
-model.add(Conv2D(filters = 8,
+model.add(Conv2D(filters = 256,
                  kernel_size = (3, 3),
                  activation = 'relu',
                  input_shape = [28,28,3],
@@ -62,22 +62,27 @@ model.add(Conv2D(filters = 8,
 # 建立卷積層，filter=64,即 output size, Kernal Size: 3x3, activation function 採用 relu
 #model.add(Conv2D(64, (3, 3), activation='relu'))
 # 建立池化層，池化大小=2x2，取最大值
+model.add(Conv2D(256, (3, 3), activation='relu', padding = 'same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(16, (3, 3), activation='relu'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(Conv2D(1028, (3, 3), activation='relu', padding = 'same'))
+model.add(Conv2D(1028, (3, 3), activation='relu', padding = 'same'))
+model.add(Conv2D(1028, (3, 3), activation='relu', padding = 'same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 #model.add(Conv2D(64, (3, 3), activation='relu'))
 #model.add(MaxPooling2D(pool_size=(2, 2)))
 # Flatten層把多維的輸入一維化，常用在從卷積層到全連接層的過渡。
 model.add(Flatten())
 # 全連接層: 128個output
-model.add(Dense(16, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(64, activation='relu'))
 # Dropout層隨機斷開輸入神經元，用於防止過度擬合，斷開比例:0.5
-#model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 # 使用 softmax activation function，將結果分類
-model.add(Dense(1, activation='softmax'))
+model.add(Dense(7, activation='softmax'))
 
 # 編譯: 選擇損失函數、優化方法及成效衡量方式
 model.compile(loss='categorical_crossentropy',
@@ -85,17 +90,17 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # 進行訓練, 訓練過程會存在 train_history 變數中
-train_history = model.fit(x_train, y_train,
+train_history = model.fit(X_img_train_normalize, y_label_train_OneHot,
                           validation_split = 0.2,
                           batch_size = 10,
                           epochs = 12,
                           verbose = 1)
-
+'''
 # 顯示損失函數、訓練成果(分數)
 score = model.evaluate(x_test, y_test, verbose = 0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-
+'''
 # Design your CNN model
 # 使用最後的模型進行測試資料預測
 load_test_img = pd.read_csv('data/project3_test.csv')
@@ -105,4 +110,4 @@ x_test_normalize = x_test.astype('float32') / 255.0
 df_submit = pd.DataFrame([], columns=['Id', 'Label'])
 df_submit['Id'] = [f'{i:04d}' for i in range(len(x_test_normalize))]
 df_submit['Label'] = np.argmax(model.predict(x_test_normalize), axis=-1)
-df_submit.to_csv('submission_CNN.csv', index=None)
+df_submit.to_csv('data/predict.csv', index=None)
